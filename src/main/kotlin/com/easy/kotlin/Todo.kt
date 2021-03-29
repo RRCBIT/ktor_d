c895bb862cff1f1013b2359e3c6a0d43625fda63
YChi_Lu <luyege+github@gmail.com>
2021-03-29 15:05:01 -0500

    'data_class'

---
+++ src/main/kotlin/com/easy/kotlin/Todo.kt
@@ -0,0 +1,19 @@
+package com.easy.kotlin
+
+data class Todo (
+    val id: String?,
+    val url: String?,
+    val title: String?,
+    val order: Int?,
+    val completed: Boolean? = false
+) {
+    fun merge(newTodo: Todo): Todo {
+        return Todo(
+            id,
+            newTodo.url ?: url,
+            newTodo.title ?: title,
+            newTodo.order ?: order,
+            newTodo.completed ?: completed
+        )
+    }//end of merge
+}
