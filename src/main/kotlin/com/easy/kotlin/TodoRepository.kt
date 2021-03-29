f347be6b165c07ce67ae42a8888d6dbe8f6917b1
ine <luyege+github@gmail.com>
2021-03-29 15:35:14 -0500

    'RepoPattern'

---
+++ src/main/kotlin/com/easy/kotlin/TodoRepository.kt
@@ -0,0 +1,31 @@
+package com.easy.kotlin
+
+import java.util.*
+
+class TodoRepository {
+    private var todos = mutableMapOf<String, Todo>()
+    private val baseUrl = "http://localhost/"
+    fun save(todo: Todo): Todo {
+        val randomUUID = UUID.randomUUID()
+        val newId = "$randomUUID"
+        val updatedTodo = todo.copy(id = newId, url = "$baseUrl$randomUUID", order = todo.order ?: 0)
+        todos[newId] = updatedTodo
+        return updatedTodo
+    }
+
+    fun getAll(): Set<Todo> = todos.values.toSet()
+
+    fun get(id: String): Todo? = todos.get(id)
+
+    fun deleteAll() = todos.clear()
+
+    fun delete(id: String): Todo? = todos.remove(id)
+
+    fun patch(id: String, todo: Todo): Todo? {
+        return todos[id]?.let {
+            val newTodo = todos[id]?.merge(todo)
+            newTodo?.let { a -> todos[id] = a }
+            newTodo
+        }
+    }
+}
