7c446152cf48f98442c221a054fed6f1eb5dd19a
ine <luyege+github@gmail.com>
2021-04-01 16:14:58 -0500

    test

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,7 +1,9 @@
 package com.jetbrains.handson.httpapi
 
 import io.ktor.application.Application
-
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.serialization.json
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -9,4 +11,5 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    registerCustomerRoutes()
 }
--- src/main/kotlin/com/jetbrains/handson/httpapi/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/CustomerRoutes.kt
@@ -1,5 +1,9 @@
 package com.jetbrains.handson.httpapi
 
+import io.ktor.application.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
+import io.ktor.response.*
 import io.ktor.http.*
 import io.ktor.routing.*
 
@@ -16,10 +20,23 @@ fun Route.customerRouting() {
 
         }
         post {
-
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
         }
         delete("{id}") {
-
+            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
+            if (customerStorage.removeIf { it.id == id }) {
+                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("Not Found", status = HttpStatusCode.NotFound)
+            }
         }
+    }
+}
+
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
     }
 }
