24218f0951dffe5f206802f34f7a15a68b78a2f7
ine <luyege+github@gmail.com>
2021-04-03 17:02:29 -0500

    Application-kt

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,13 +1,16 @@
 package com.jetbrains.handson.httpapi
 
+import com.jetbrains.handson.httpapi.registerCustomerRoutes
+import com.jetbrains.handson.httpapi.registerOrderRoutes
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.serialization.json
 
+
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-fun Application.module() {
+fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         json()
     }
